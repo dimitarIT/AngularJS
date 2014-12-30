860d325ca509f927bfa0927c8c46a92f2047fc77
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2014-12-30 19:26:38 +0200

    added dependencies

--- Ads – AngularJS Practical Project/app/js/adsApp.js
+++ Ads – AngularJS Practical Project/app/js/adsApp.js
@@ -1,3 +1,29 @@
 'use strict';
 
-var adsApp = angular.module('adsApp', []); //Todo add dependencies
+var adsApp = angular.module('adsApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'adsAppControllers'
+]);
+
+adsApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'templates/login.html'
+            }).
+            when('/register', {
+                templateUrl: 'templates/register.html',
+                controller: 'RegisterController'
+            }).
+            when('/home', {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+    }
+])
+    .constant('baseUrl', 'http://softuni-ads.azurewebsites.net/api');
