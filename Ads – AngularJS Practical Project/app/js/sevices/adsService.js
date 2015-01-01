a8e405ac7aecc4aedc5580ebd58bebe1a7ce01ac
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2015-01-01 18:36:40 +0200

    added service

--- Ads – AngularJS Practical Project/app/js/sevices/adsService.js
+++ Ads – AngularJS Practical Project/app/js/sevices/adsService.js
@@ -1,3 +1,60 @@
-/**
- * Created by DimitarIT on 30.12.2014 г..
- */
+'use strict';
+
+adsApp.factory('adsData', function adsData($http, $q, baseUrl) {
+    function getAllAdds(pageNumber, townId, categoryId) {
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: baseUrl + '/ads?pagesize=5&startpage=' + pageNumber + '&TownId=' + townId + '&CategoryId=' + categoryId
+        })
+            .success(function(data, status, headers, config) {
+                deferred.resolve(data, status, headers, config);
+            })
+            .error(function(data, status, headers, config) {
+                deferred.reject(data, status, headers, config);
+            });
+
+        return deferred.promise;
+    }
+
+    function getAllAdsByTown(townId, categoryId, pageNumber) {
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: baseUrl + '/ads?pagesize=5&TownId=' + townId + '&CategoryId=' + categoryId + '&startpage=' + pageNumber
+        })
+            .success(function(data, status, headers, config) {
+                deferred.resolve(data, status, headers, config);
+            })
+            .error(function(data, status, headers, config) {
+                deferred.reject(data, status, headers, config);
+            });
+
+        return deferred.promise;
+    }
+
+    function getAllAdsByCAtegory(categoryId, townId,  pageNumber) {
+        var deferred = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: baseUrl + '/ads?pagesize=5&CategoryId=' + categoryId + '&TownId=' + townId + '&startpage=' + pageNumber
+        })
+            .success(function(data, status, headers, config) {
+                deferred.resolve(data, status, headers, config);
+            })
+            .error(function(data, status, headers, config) {
+                deferred.reject(data, status, headers, config);
+            });
+
+        return deferred.promise;
+    }
+
+    return{
+        getAll: getAllAdds,
+        getByTown: getAllAdsByTown,
+        getByCategory: getAllAdsByCAtegory
+    };
+});
