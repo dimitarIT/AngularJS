16803e8ffd26e1033eb3c7cfc67944796642021f
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2015-01-07 21:06:38 +0200

    - register functionality almost done

--- Ads – AngularJS Practical Project/app/js/controllers/RegisterController.js
+++ Ads – AngularJS Practical Project/app/js/controllers/RegisterController.js
@@ -3,6 +3,30 @@
 var adsAppControllers = adsAppControllers || angular.module('adsAppControllers', []);
 
 adsAppControllers.controller('RegisterController',
-    function registerController () {
+    function registerController($scope, $rootScope, townsData,
+                                authenticationService, authorizationService) {
 
+        $scope.registrationActive = true;
+
+        townsData.getAll()
+            .then(function (data) {
+                $scope.townsData = data;
+            }, function (error) {
+                $rootScope.$broadcast('errorHandle');
+            });
+
+        $scope.register = function (credentials, registerForm) {
+            if (registerForm.$valid) {
+                authenticationService.register(credentials)
+                    .then(function (data) {
+                        authorizationService.setUserSession(data);
+                        $scope.registrationActive = false;
+                        $rootScope.$broadcast('operatonSuccessfull', 'User account created.Please login');
+                    }, function (error) {
+                        // TODO;
+                    });
+            }
+        };
+
+        // TODO: handle errors with registration data
     });
