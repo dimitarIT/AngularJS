ce55320f6d2ca88ad32aea4bdf38de1d736b7c9d
Dimitar Keremedchiev <d.k.keremedchiev@gmail.com>
2015-01-10 22:55:24 +0200

    - some errors handlers added - some bugs fixed - refactoring

--- Ads – AngularJS Practical Project/app/js/controllers/UserProfileController.js
+++ Ads – AngularJS Practical Project/app/js/controllers/UserProfileController.js
@@ -1,42 +1,44 @@
 'use strict';
 
-// TODO: ADD Errors
 var adsAppControllers = adsAppControllers || angular.module('adsAppControllers', []);
 
 adsAppControllers.controller('UserProfileController',
-    function userProfileController($scope, $rootScope, $route, userProfile, townsData) {
-        var errorMessage;
+    function userProfileController($scope, $rootScope, $route,
+                                   userProfile, townsData, notifyService) {
 
-        /* get towns for dropdown */
-        townsData.getAll().then(function (data) {
-            $scope.townsData = data;
-        }, function (error) {
-            $rootScope.$broadcast('alertMessage');
-        });
+        // Load towns for drop down
+        townsData.getAll()
+            .then(function (data) {
+                $scope.townsData = data;
+            }, function (error) {
+                notifyService.showError('Cannot load towns', error);
+            });
 
-        userProfile.getProfile().then(function (data) {
-            $scope.currentuser = data;
-            $scope.editProfileForm = {
-                name: data.name,
-                email: data.email,
-                phoneNumber: data.phoneNumber ? data.phoneNumber : '',
-                townId: data.townId ? data.townId : null
-            };
-        }, function (error) {
-            $rootScope.$broadcast('alertMessage');
-        });
+        userProfile.getProfile()
+            .then(function (data) {
+                $scope.currentuser = data;
+                $scope.editProfileForm = {
+                    name: data.name,
+                    email: data.email,
+                    phoneNumber: data.phoneNumber ? data.phoneNumber : '',
+                    townId: data.townId ? data.townId : null
+                };
+            }, function (error) {
+                notifyService.showError('Cannot load user profile', error);
+            });
 
         $scope.updateProfile = function (editProfileForm) {
             if (!editProfileForm.name || !editProfileForm.email || !editProfileForm.phoneNumber) {
                 return;
             }
 
-            userProfile.ediProfile(editProfileForm).then(function (data) {
-                $route.reload();
-                $rootScope.$broadcast('alertMessage', data.message);
-            }, function (error) {
-                $rootScope.$broadcast('alertMessage');
-            });
+            userProfile.ediProfile(editProfileForm)
+                .then(function (data) {
+                    $route.reload();
+                    notifyService.showInfo('Your profile is update', data.message);
+                }, function (error) {
+                    notifyService.showError('Cannot edit user profile', error);
+                });
         };
 
         $scope.changePassword = function (credentials, changePasswordForm) {
@@ -44,23 +46,12 @@ adsAppControllers.controller('UserProfileController',
                 return;
             }
 
-            userProfile.changePassword(credentials).then(function (data) {
-                $route.reload();
-                $rootScope.$broadcast('alertMessage', data.message);
-            }, function (error) {
-                errorMessage = error.modelState;
-                handleErrorMessage(errorMessage);
-            });
+            userProfile.changePassword(credentials)
+                .then(function (data) {
+                    $route.reload();
+                    notifyService.showInfo('Your password is changed', data.message);
+                }, function (error) {
+                    notifyService.showError('Cannot edit user password', error);
+                });
         };
-
-        /* hdnle errors with change password data */
-        function handleErrorMessage(errorMessage) {
-            if (errorMessage['']) {
-                $rootScope.$broadcast('alertMessage', errorMessage[''][0]);
-            } else if (errorMessage['model.ConfirmPassword']) {
-                $rootScope.$broadcast('alertMessage', errorMessage['model.ConfirmPassword'][0]);
-            } else {
-                $rootScope.$broadcast('alertMessage');
-            }
-        }
     });
